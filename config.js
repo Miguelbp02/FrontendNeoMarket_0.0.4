5b16cd3e9d5c86ac10e139fbce7fef3228edfa99
Miguel Busto Porta <miguelcaldas93@gmail.com>
2026-02-25 00:03:00 +0100

    Update API_BASE URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // config.js
 // Cambia esta URL cada vez que el backend (Cloudflare Tunnel) cambie de dirección
 // Ejemplo: 'https://xxxxxx.trycloudflare.com'
-window.API_BASE = 'urldeldia';
+window.API_BASE = 'https://specialties-nodes-emphasis-actually.trycloudflare.com';
+
